a7f2266a62015263509fcb821e26982067f930ed
Sukhbinder Singh <sukh2010@yahoo.com>
2024-06-18 08:18:19 +0530

    Create clipcon.py

---
+++ pbpastesrc/clipcon.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+
+def get_clipboard_content_tkinter():
+    root = tk.Tk()
+    root.withdraw()  # Hide the window
+    clipboard_content = root.clipboard_get()
+    root.destroy()
+    print(clipboard_content)
+
+
+if __name__ == "__main__":
+    get_clipboard_content_tkinter()
