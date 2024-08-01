7d5aebdbcbfbb93bf8ec286ddcea8a9f7ff59dd1
Sukhbinder Singh <sukh2010@yahoo.com>
2024-08-01 19:40:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A tiny uility to mimic pbpaste in windows.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/sukhbinder/pbpaste-tkinter",
+    url="https://github.com/sukhbinder/pbpaste-win",
     packages=setuptools.find_packages(),
     install_requires=[],
     entry_points={"console_scripts": ["pbpaste = pbpastesrc.clipcon:get_clipboard_content_tkinter"]}
