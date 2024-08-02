ea85658ef186488cd460c9de848a552cd66835ba
Sukhbinder Singh <sukh2010@yahoo.com>
2024-08-02 19:38:38 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pbpaste-win",
-    version="0.1.0",
+    version="0.1.1",
     author="Sukhbinder Singh",
     author_email="sukh2010@yahoo.com",
     description="A tiny uility to mimic pbpaste in windows.",
